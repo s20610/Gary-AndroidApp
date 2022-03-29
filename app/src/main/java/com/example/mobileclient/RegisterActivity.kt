f92d09794e13da1006bfbb6eb40dd489cc7bd056
borys <borys.kotnowski@gocloud.pl>
2022-03-29 22:18:08 +0200

    Co-authored-by: Aleksandra Mira <s20383@pjwstk.edu.pl>
    Redesigned registration_form.xml with Material Components.
    Changed date picker in RegisterActivity.kt to Material date picker.
    Created splash_screen.xml and login_form.xml

--- app/src/main/java/com/example/mobileclient/RegisterActivity.kt
+++ app/src/main/java/com/example/mobileclient/RegisterActivity.kt
@@ -1,13 +1,13 @@
 package com.example.mobileclient
 
-import android.app.DatePickerDialog
 import android.os.Bundle
-import android.view.View
-import android.view.View.OnFocusChangeListener
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointBackward
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.datepicker.MaterialDatePicker.INPUT_MODE_TEXT
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class RegisterActivity : AppCompatActivity() {
@@ -15,29 +15,19 @@ class RegisterActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registration_form)
-        var birthdayInput : EditText= findViewById(R.id.birthdayInput)
-        birthdayInput.showSoftInputOnFocus = false
-        val calendar = Calendar.getInstance()
-
-        birthdayInput.onFocusChangeListener = View.OnFocusChangeListener{ view, focused ->
-            if(focused) {
-                var day = calendar.get(Calendar.DAY_OF_MONTH)
-                var month = calendar.get(Calendar.MONTH)
-                var year = calendar.get(Calendar.YEAR)
-                val picker = DatePickerDialog(
-                    this,
-                    DatePickerDialog.OnDateSetListener { datePicker, mYear, mMonth, mDay ->
-                        birthdayInput.setText("" + mDay + "/" + mMonth + "/" + mYear)
-                    },
-                    year,
-                    month,
-                    day
-                )
-                picker.show()
-                (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).apply {
-                    hideSoftInputFromWindow(birthdayInput.windowToken, 0)
-                }
-            }
+        val birthdayInput: TextInputEditText = findViewById(R.id.birthday_input)
+        val constraintBuilder =
+            CalendarConstraints.Builder().setValidator(DateValidatorPointBackward.now())
+        val datePicker = MaterialDatePicker.Builder.datePicker().setTitleText("Your birth date")
+            .setInputMode(INPUT_MODE_TEXT).setCalendarConstraints(constraintBuilder.build()).build()
+        datePicker.addOnPositiveButtonClickListener {
+            birthdayInput.setText(datePicker.headerText)
+        }
+        datePicker.addOnCancelListener {
+            datePicker.dismiss()
+        }
+        birthdayInput.setOnClickListener {
+            datePicker.show(supportFragmentManager, "birthdate_picker")
         }
     }
 }

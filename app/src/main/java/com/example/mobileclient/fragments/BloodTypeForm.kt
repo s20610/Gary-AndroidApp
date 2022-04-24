13d0505dd8d090fde701671b84723c4158a7b4b2
Aleksandra Mira <s20383@pjwstk.edu.pl>
2022-04-24 21:35:06 +0200

    Add Blood type form

--- app/src/main/java/com/example/mobileclient/fragments/BloodTypeForm.kt
+++ app/src/main/java/com/example/mobileclient/fragments/BloodTypeForm.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import com.example.mobileclient.R
-import com.example.mobileclient.databinding.FragmentGuestScreenBinding
-import com.example.mobileclient.databinding.FragmentMedicalInfoMainBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,17 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MedicalInfoMain.newInstance] factory method to
+ * Use the [BloodTypeForm.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MedicalInfoMain : Fragment() {
+class BloodTypeForm : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var _binding: FragmentMedicalInfoMainBinding? = null
-    // This property is only valid between onCreateView and
-// onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,30 +35,7 @@ class MedicalInfoMain : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentMedicalInfoMainBinding.inflate(inflater, container, false)
-        val view = binding.root
-        binding.topAppBar.setNavigationOnClickListener {
-            binding.drawerLayout.open()
-            binding.navigationView.setCheckedItem(R.id.nav_user_details)
-        }
-        binding.imageView.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_medicalInfoMain_to_bloodTypeForm)
-        }
-        binding.navigationView.getHeaderView(0).setOnClickListener {
-            Navigation.findNavController(view)
-                .navigate(R.id.action_medicalInfoMain_to_userInfo)
-
-        }
-        binding.navigationView.setNavigationItemSelectedListener {
-            it.isChecked =true
-            if(it.toString() == "Tutorials"){
-                Navigation.findNavController(view)
-                    .navigate(R.id.action_medicalInfoMain_to_loggedInScreen)
-            }
-            binding.drawerLayout.close()
-            true
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_blood_type_form, container, false)
     }
 
     companion object {
@@ -75,12 +45,12 @@ class MedicalInfoMain : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MedicalInfoMain.
+         * @return A new instance of fragment BloodTypeForm.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MedicalInfoMain().apply {
+            BloodTypeForm().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)

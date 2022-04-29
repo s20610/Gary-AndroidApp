457f35fc1c89f6978cf608314850d6764f94a21a
Wiktor Czech <s21291@pjwstk.edu.pl>
2022-04-29 15:30:06 +0200

    Poprawiony formularz dodawania zdarzenia - wysuwana lista przykładowych zdarzeń

--- app/src/main/java/com/example/mobileclient/fragments/Incident.kt
+++ app/src/main/java/com/example/mobileclient/fragments/Incident.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.mobileclient.R
 import com.example.mobileclient.databinding.FragmentIncidentBinding
 import com.example.mobileclient.databinding.FragmentLoginBinding
@@ -36,23 +39,29 @@ class Incident : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         _binding = FragmentIncidentBinding.inflate(inflater, container, false)
+
+        val incidents = arrayOf("Nieprzytomna osoba", "Zadławienie", "Rana kłóta", "Wypadek samochodowy", "Inne")
+        val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, incidents)
+        binding.autoCompleteTextView2.setAdapter(arrayAdapter)
+
         val view = binding.root
 
         binding.button2.setOnClickListener{
-            Navigation.findNavController(view)
-                .navigate(R.id.action_incident_to_loggedInScreen)
+            Navigation.findNavController(view).navigate(R.id.action_incident_to_loggedInScreen)
+
         }
-
         return view
-
     }
 
     companion object {
